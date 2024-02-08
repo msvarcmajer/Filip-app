aed7c4a3019c21166536c191fb9d3a3f5360aab3
msvarcmajer <49949240+msvarcmajer@users.noreply.github.com>
2024-02-08 13:02:40 +0100

    registration fix

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,12 +1,13 @@
 <template>
-  <div style="z-index: 999;">
-    <h2>Registration</h2>
+  <div class="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-75 z-50">
     <!-- Conditionally display the RegistrationForm based on showRegistrationForm -->
-    <RegistrationForm @registration-success="handleRegistrationSuccess" v-if="showRegistrationForm" />
-
-    <!-- Display a success message when registration is successful -->
-    <div v-if="registrationSuccessful">
-      <p>Registration successful!</p>
+    <div class="bg-white w-full max-w-md p-6 rounded-lg shadow-lg">
+      <button @click="closeRegistrationForm" class="close-button absolute top-0 right-0 m-4">&times;</button>
+      <RegistrationForm @registration-success="handleRegistrationSuccess" v-if="showRegistrationForm" />
+      <!-- Display a success message when registration is successful -->
+      <div v-if="registrationSuccessful">
+        <p>Registration successful!</p>
+      </div>
     </div>
   </div>
 </template>
@@ -29,13 +30,24 @@ export default {
       // Handle the event emitted by the RegistrationForm
       // Update the data property to close the form and show success message
       this.registrationSuccessful = true;
-      
       // Close the registration form
       this.showRegistrationForm = false;
-
       // Use Vue Router to navigate to the chat page
       this.$router.push({ name: 'Chat' });
+    },
+    closeRegistrationForm() {
+      // Method to close the registration form
+      this.showRegistrationForm = false;
     },
   },
 };
 </script>
+
+<style scoped>
+.close-button {
+  background: none;
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+}
+</style>
